8444f893b4ecc71da63487c9b5ed4892ec53df5f
unknown <arturasstupak@gmail.com>
2022-03-01 19:24:36 +0200

    gitignore create

--- subscribe/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-echo '<h1>Registracija</h1>';
-
-echo '<form action="helper.php" method="post">';
-echo '<input type="text" name="email" placeholder="emailas">';
-echo '<input type="submit" value="Registruotis">';
-echo '</form>';
